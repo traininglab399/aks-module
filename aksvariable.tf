aeccf5e6f8303a42755d7acb5ef4787b6d345486
traininglab399 <traininglab399@gmail.com>
2025-02-10 19:29:07 +0530

    Create aksvariable.tf

---
+++ aksvariable.tf
@@ -0,0 +1,53 @@
+variable "resource_group_name" {
+  description = "The name of the resource group where the AKS cluster will be created."
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "The location of the resource group."
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "The name of the AKS cluster."
+  type        = string
+}
+
+variable "dns_prefix" {
+  description = "The DNS prefix for the AKS cluster."
+  type        = string
+}
+
+variable "node_count" {
+  description = "The number of nodes in the default node pool."
+  type        = number
+  default     = 2
+}
+
+variable "vm_size" {
+  description = "The size of the virtual machines in the node pool."
+  type        = string
+  default     = "Standard_DS2_v2"
+}
+
+variable "subnet_id" {
+  description = "The ID of the Subnet to attach to the AKS cluster."
+  type        = string
+}
+
+variable "log_analytics_workspace_id" {
+  description = "The ID of the Log Analytics Workspace for monitoring."
+  type        = string
+}
+
+variable "enable_private_cluster" {
+  description = "Enable private cluster configuration."
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A map of tags to apply to the resources."
+  type        = map(string)
+  default     = {}
+}
