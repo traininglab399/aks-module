a20f03cbca88b8b8d43c7fd76ffad245461079ae
traininglab399 <traininglab399@gmail.com>
2025-02-10 19:16:53 +0530

    Create aks.tf

---
+++ aks.tf
@@ -0,0 +1,28 @@
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.cluster_name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name           = "default"
+    node_count     = var.node_count
+    vm_size        = var.vm_size
+    vnet_subnet_id = var.subnet_id
+  }
+
+  network_profile {
+    network_plugin     = "azure"
+    service_cidr       = "10.1.0.0/16"  # Updated Service CIDR
+    dns_service_ip     = "10.1.0.10"   # Must be within the Service CIDR
+    docker_bridge_cidr = "172.17.0.1/16"
+    outbound_type      = "loadBalancer"  # Moved from the second block
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  private_cluster_enabled = var.enable_private_cluster
+  tags                     = var.tags
+}
