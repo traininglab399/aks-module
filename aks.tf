0b97eb51c3e93e1b0194f34ff13769b5afeea722
traininglab399 <traininglab399@gmail.com>
2025-02-13 14:20:13 +0530

    Update aks.tf

--- aks.tf
+++ aks.tf
@@ -8,15 +8,15 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     name           = "default"
     node_count     = var.node_count
     vm_size        = var.vm_size
-    vnet_subnet_id = var.subnet_id
+    vnet_subnet_id = var.subnet_id # Subnet ID passed from the root module
   }
 
   network_profile {
     network_plugin     = "azure"
-    service_cidr       = "10.1.0.0/16"  # Updated Service CIDR
+    service_cidr       = "10.1.0.0/16" # Service CIDR (non-overlapping with VNet)
     dns_service_ip     = "10.1.0.10"   # Must be within the Service CIDR
     docker_bridge_cidr = "172.17.0.1/16"
-    outbound_type      = "loadBalancer"  # Moved from the second block
+    outbound_type      = var.enable_private_cluster ? "userDefinedRouting" : "loadBalancer"
   }
 
   identity {
@@ -24,5 +24,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   }
 
   private_cluster_enabled = var.enable_private_cluster
-  tags                     = var.tags
+
+  tags = var.tags
 }
+
