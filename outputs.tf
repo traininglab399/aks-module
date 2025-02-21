4c258a5cb5a478f5e87d09e9f2b19d9c01e1b7f4
traininglab399 <traininglab399@gmail.com>
2025-02-21 14:26:34 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,30 @@
+output "resource_group_name" {
+  description = "The name of the resource group."
+  value       = local.main_resource_group_name
+}
+
+output "vnet_name" {
+  description = "The name of the Virtual Network."
+  value       = local.vnet_name
+}
+
+output "subnet_id" {
+  description = "The ID of the subnet used for AKS."
+  value       = data.azurerm_subnet.aks_subnet.id
+}
+
 output "cluster_name" {
   description = "The name of the AKS cluster."
-  value       = azurerm_kubernetes_cluster.aks_cluster.name
+  value       = module.aks.cluster_name
 }
 
 output "kube_config" {
   description = "The kubeconfig file content for accessing the AKS cluster."
-  value       = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+  value       = module.aks.kube_config
   sensitive   = true
 }
 
 output "cluster_fqdn" {
   description = "The FQDN of the AKS cluster."
-  value       = azurerm_kubernetes_cluster.aks_cluster.fqdn
+  value       = module.aks.cluster_fqdn
 }
